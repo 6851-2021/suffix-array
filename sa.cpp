6b24f759a151221f80c6d3c4310a01e792991197
Aleksandar Krastev <alexalex@mit.edu>
2021-04-28 18:37:45 -0400

    Implemented nlogn suffix array computation

--- sa.cpp
+++ sa.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-#include <string>
 #include <algorithm>
+#include <array>
+#include <iostream>
+#include <map>
+#include <string>
 #include <vector>
 
 struct SuffixArray {
@@ -8,6 +10,9 @@ struct SuffixArray {
     std::vector<unsigned> lcp;
 };
 
+//
+// Naive algorithm
+//
 unsigned naive_lcp(const std::vector<unsigned> &s, unsigned i, unsigned j) {
     unsigned l=0;
     while (s[i + l] == s[j + l]) {
@@ -43,6 +48,139 @@ SuffixArray compute_naive(const std::vector<unsigned> &s) {
     }
     return res;
 }
+
+//
+// Shared Utilities
+//
+
+template <typename T>
+std::ostream& operator<<(std::ostream &s, const std::vector<T> &v) {
+    for (const auto &x : v) {
+        s << x << " ";
+    }
+    return s;
+}
+
+std::vector<unsigned> sort_chars(const std::vector<unsigned> &s) {
+    std::map<unsigned, unsigned> charPtr;
+    for (unsigned c : s) {
+        charPtr[c]++;
+    }
+
+    unsigned prefixSum = 0;
+    for (auto &kv : charPtr) {
+        unsigned newSum = prefixSum + kv.second;
+        kv.second = prefixSum;
+        prefixSum = newSum;
+    }
+
+    std::vector<unsigned> res(s.size());
+    for (unsigned i = 0;i < s.size();i++) {
+        auto it = charPtr.find(s[i]);
+        res[(it->second)++] = i;
+    }
+    return res;
+}
+
+// Return an vector of the indices of the elements in sorted order
+template <size_t K>
+void radix_sort(
+    const std::vector<std::array<unsigned, K>> &v,
+    std::vector<unsigned> &ord
+) {
+    // (hopefully) reuse memory across function calls
+    static std::vector<unsigned> bPtr;
+    static std::vector<unsigned> newOrd;
+
+    ord.resize(v.size());
+    for (unsigned i = 0;i < v.size();i++) {
+        ord[i] = i;
+    }
+
+    for (unsigned i = K - 1;i < K;i--) {
+        // TODO see if it makes sense to just skip the first iteration
+        bPtr.resize(v.size());
+        std::fill(bPtr.begin(), bPtr.end(), 0);
+        for (const auto &x : v) {
+            bPtr[x[i]]++;
+        }
+
+        unsigned prefSum = 0;
+        for (unsigned j = 0;j < v.size();j++) {
+            unsigned newSum = prefSum + bPtr[j];
+            bPtr[j] = prefSum;
+            prefSum = newSum;
+        }
+
+        newOrd.resize(v.size());
+        for (unsigned j = 0;j < ord.size();j++) {
+            unsigned bInd = v[ord[j]][i];
+            //std::cerr << "        " << j << " -> b=" << bInd << "; ind="  << bPtr[bInd] << "\n";
+            newOrd[bPtr[bInd]++] = ord[j];
+        }
+
+        // O(1) because of C++ being smart
+        std::swap(ord, newOrd);
+        //std::cerr << "    ord at i=" << i << ": " << ord << "\n";
+    }
+}
+
+template <typename T>
+void get_rank(
+    const std::vector<T> &v,
+    const std::vector<unsigned> &ord,
+    std::vector<unsigned> &rank
+) {
+    rank.resize(ord.size());
+    if (v.empty()) {
+        return;
+    }
+    rank[0] = 0;
+
+    unsigned r = 0;
+    for (unsigned i = 1;i < ord.size();i++) {
+        if (v[ord[i]] != v[ord[i - 1]]) {
+            r = i;
+        }
+        rank[ord[i]] = r;
+    }
+}
+
+//
+// NlogN
+//
+
+SuffixArray compute_nlogn(const std::vector<unsigned> &input) {
+    static std::vector<std::array<unsigned, 2>> sortArr;
+    static std::vector<unsigned> rank;
+
+    SuffixArray sa;
+    sa.ind = sort_chars(input);
+    get_rank(input, sa.ind, rank);
+
+    for (unsigned stride = 1;stride < input.size();stride *= 2) {
+        //std::cerr << "before stride=" << stride << "\n";
+        //std::cerr << "ind: " << sa.ind << "\n";
+        //std::cerr << "rank: " << rank << "\n";
+
+        sortArr.resize(input.size());
+        for (unsigned i = 0;i < input.size();i++) {
+            unsigned j = i + stride;
+            j = j >= input.size() ? j - input.size() : j;
+
+            sortArr[i][0] = rank[i];
+            sortArr[i][1] = rank[j];
+        }
+        radix_sort(sortArr, sa.ind);
+        get_rank(sortArr, sa.ind, rank);
+    }
+    return sa;
+}
+
+
+//
+// Infrastructure
+//
 
 enum AlgoT {
     NAIVE,
@@ -134,14 +272,8 @@ std::vector<unsigned> read_input() {
 }
 
 void print_arr(const SuffixArray &arr) {
-    for (const unsigned &v : arr.ind) {
-        std::cout << v << " ";
-    }
-    std::cout << "\n";
-    for (const unsigned &v : arr.lcp) {
-        std::cout << v << " ";
-    }
-    std::cout << "\n";
+    std::cout << arr.ind << "\n";
+    //std::cout << arr.lcp << "\n";
 }
 
 int main(int argc, char* argv[]) {
@@ -152,11 +284,15 @@ int main(int argc, char* argv[]) {
     Options opt = parse_args(args);
 
     std::vector<unsigned> input = read_input();
+
     SuffixArray arr;
     for (unsigned i = 0;i < opt.repeatCnt;i++) {
         switch(opt.algorithm) {
         case NAIVE:
             arr = compute_naive(input);
+            break;
+        case NLOGN:
+            arr = compute_nlogn(input);
             break;
         default:
             std::cerr << "Algorithm not yet supported\n";
